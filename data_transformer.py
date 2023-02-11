b26f746b0cfb3d3e768a3855b656ea302f75f63d
Rohan Awhad <rohanavad007@gmail.com>
2023-02-11 18:19:08 +0530

    removed everything. added only chunker

--- data_transformer.py
+++ data_transformer.py
@@ -1,12 +1,9 @@
-import json
-
+# chunk text in each article present in data
 from loguru import logger
 from nltk.tokenize import sent_tokenize
 
-from db import DB, Article, Chunk
-
-# Chunking text
 def get_chunks(text, max_len_chars = 1800):
+    assert len(text) > 0, 'Text is empty'
     curr_size = 0
     chunks = [[]]
     for sent in sent_tokenize(text):
@@ -19,30 +16,7 @@ def get_chunks(text, max_len_chars = 1800):
             curr_size = 0
             
     if curr_size: chunks[-1] = ' '.join(chunks[-1])
+    chunks = [c for c in chunks if len(c) > 0 and isinstance(c, str)]
+
     logger.info(f'Created {len(chunks)} chunks')
     return chunks
-
-def loader():
-    # load the data
-    with open('data/raw.json') as f:
-        data = json.load(f)
-
-    for uid, value in data.items():
-        logger.info(f'Processing {uid}')
-        text = value['text'].replace('"', "'")
-        chunks = get_chunks(text)
-
-        with DB:
-            article, _ = Article.get_or_create(
-                uid=uid,
-                content=text,
-                url=value['link'],
-                title=value['title']
-            )
-
-            for chunk in chunks: Chunk.get_or_create(article=article, chunk=chunk)
-
-        logger.info(f'loaded {uid}')
-
-if __name__ == '__main__':
-    loader()
