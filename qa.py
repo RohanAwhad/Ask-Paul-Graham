abc8a444de5aa1a6ec73c1035bb6549a365e6fb2
Rohan Awhad <rohanavad007@gmail.com>
2023-02-11 19:15:25 +0530

    added answer generator

---
+++ qa.py
@@ -0,0 +1,35 @@
+import torch
+
+from loguru import logger
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+model_name = 'vblagoje/bart_lfqa'
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model.eval()
+
+
+def get_answer(question: str, documents: list[str]) -> str:
+    logger.info(f'Generating answer for question: {question}')
+    logger.info(f'Using documents: {documents}')
+    conditioned_doc = "<P> " + " <P> ".join(documents)
+    question_and_doc = f'question: {question} context: {conditioned_doc}'
+    model_input = tokenizer(question_and_doc, return_tensors='pt', truncation=True)
+    with torch.no_grad():
+        model_output = model.generate(input_ids=model_input["input_ids"],
+                                            attention_mask=model_input["attention_mask"],
+                                            min_length=64,
+                                            max_length=256,
+                                            do_sample=False, 
+                                            early_stopping=True,
+                                            num_beams=3,
+                                            temperature=1.0,
+                                            top_k=None,
+                                            top_p=None,
+                                            eos_token_id=tokenizer.eos_token_id,
+                                            no_repeat_ngram_size=3,
+                                            num_return_sequences=1)
+
+    answer = tokenizer.decode(model_output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+    logger.info(f'Answer: {answer}')
+    return answer
