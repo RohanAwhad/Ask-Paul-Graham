8eeb4e20c5ad77a02c8aff2784737d17956c51d8
Rohan Awhad <rohanavad007@gmail.com>
2023-02-12 21:20:57 +0530

    added data saving and loading methods

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import faiss
 import json
+import numpy as np
+import os
+import pandas as pd
+
 from functools import cache
 from loguru import logger
 
@@ -6,38 +11,66 @@ import data_transformer
 import encoder
 import qa
 
+# constants
+RAW_DATA_PATH = 'data/raw.json'
+CHUNKS_PATH = 'data/chunks.parquet'
+CHUNK_EMBEDDINGS_PATH = 'data/chunk_embeddings.npy'
+CHUNK_INDEX_PATH = 'data/chunk_index.faiss'
+
 # load data
-with open('data/raw.json') as f:
+with open(RAW_DATA_PATH, 'r') as f:
     data = json.load(f)
 logger.info('Data loaded')
 
 
-# Getting chunks from all articles
-chunk_to_uid = dict()  # {uid: [chunk1, chunk2, ...]}
-try:
-    for uid, article in data.items():
-        chunks = data_transformer.get_chunks(text=article['text'], max_len_chars = 360)
-        for chunk in chunks:
-            chunk_to_uid[chunk] = uid
-except Exception as e:
-    logger.exception(e)
-logger.info('Chunks extracted')
+if os.path.exists(CHUNKS_PATH):
+    chunks_df = pd.read_parquet(CHUNKS_PATH)
+    logger.info('Chunks loaded')
+else:
+    # Getting chunks from all articles
+    chunk_to_uid = dict()  # {uid: [chunk1, chunk2, ...]}
+    try:
+        for uid, article in data.items():
+            chunks = data_transformer.get_chunks(text=article['text'], max_len_chars = 360)
+            for chunk in chunks:
+                chunk_to_uid[chunk] = uid
+    except Exception as e:
+        logger.exception(e)
+    logger.info('Chunks extracted')
+
+    chunks_df = pd.DataFrame.from_dict(chunk_to_uid, orient='index').reset_index()
+    chunks_df.columns = ['chunk', 'uid']
+    chunks_df.index.name = 'chunk_id'
+    chunks_df.to_parquet(CHUNKS_PATH)
+    logger.info('Chunks saved')
 
 
-chunk_list = list(chunk_to_uid.keys())
-idx_2_chunk = dict((i, chunk) for i, chunk in enumerate(chunk_list))  # {idx: chunk}
-chunk_to_idx = dict((chunk, i) for i, chunk in idx_2_chunk.items())  # {chunk: idx}
+if os.path.exists(CHUNK_EMBEDDINGS_PATH):
+    chunk_embeddings = np.load(CHUNK_EMBEDDINGS_PATH)
+    logger.info('Chunk embeddings loaded')
+else:
+    chunk_list = chunks_df['chunk'].tolist()
+    chunk_embeddings = encoder.encode(chunk_list)
+    logger.info('Embeddings calculated for chunks')
+    np.save(CHUNK_EMBEDDINGS_PATH, chunk_embeddings.numpy())
+    logger.info('Chunk embeddings saved')
 
-chunk_embeddings = encoder.encode(chunk_list)
-logger.info('Embeddings calculated for chunks')
-index = encoder.build_index(chunk_embeddings)
-logger.info('Chunk index built')
+
+
+if os.path.exists(CHUNK_INDEX_PATH):
+    index = faiss.read_index(CHUNK_INDEX_PATH)
+    logger.info('Chunk index loaded')
+else:
+    index = encoder.build_index(chunk_embeddings)
+    logger.info('Chunk index built')
+    faiss.write_index(index, CHUNK_INDEX_PATH)
+
 
 # Querying
 @cache
 def run(question):
     question_emb = encoder.encode(question)
-    k = 3
+    k = 8
     _, idx_mat = index.search(question_emb, k)
-    documents = [idx_2_chunk[idx] for idx in idx_mat[0]]
+    documents = [chunks_df.loc[idx, 'chunk'] for idx in idx_mat[0]]
     return qa.get_answer(question, documents)
